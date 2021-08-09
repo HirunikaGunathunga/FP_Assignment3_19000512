fd4782b99ab4cc95932975d4b8d5488b6e8f7605
HirunikaGunathunga <84180743+HirunikaGunathunga@users.noreply.github.com>
2021-08-09 21:26:55 +0530

    Update Caesar_Cipher.scala

--- src/Caesar_Cipher.scala
+++ src/Caesar_Cipher.scala
@@ -1,31 +1,16 @@
 object Caesar_Cipher extends App{
-  
-  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-
-  //We will shift our plaintext by this much
-  //Notice: shift = (# + 26) % 26 <-- This allows us to take any number (even if it is negative, or bigger than our alphabet size)
-  val shift = (scala.io.StdIn.readLine("Shift By: ").toInt + alphabet.size) % alphabet.size
-
-  //What is the code we want to encrypt/decrypt
+  val Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  val shift = (scala.io.StdIn.readLine("Shift By: ").toInt + Alphabet.size) % Alphabet.size
   val inputText = scala.io.StdIn.readLine("Secret Message: ")
-
-  //Lets Encrypt/Decrypt the code
   val outputText = inputText.map( (c: Char) => {
-
-    //We find the c char in our allowed alphabet
-    val x = alphabet.indexOf(c.toUpper)
-
-    //If the c char is in our alphabet then we encrypt it
-    //If it is not then we leave it be.
+    val x = Alphabet.indexOf(c.toUpper)
     if (x == -1){
       c
     }
     else{
-      alphabet((x + shift) % alphabet.size)
+      Alphabet((x + shift) % Alphabet.size)
     }
   })
-
-  //Print the result
   println(outputText)
 }
